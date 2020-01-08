15896199167859ffa19b60892de529efba197b62
tomerfooks <37155624+tomerfooks@users.noreply.github.com>
2020-01-08 21:08:59 +0200

    skinnier

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -1,73 +1,71 @@
 import React, { useEffect, useState } from "react"
 import { Link, Router } from "react-router-dom"
 import {
-  Card,
-  CardActions,
-  CardContent,
-  Button,
-  Typography
+    Card,
+    CardActions,
+    CardContent,
+    Button,
+    Typography
 } from "@material-ui/core"
 
 const Archive = props => {
-  const [archive, setArchive] = useState([])
-  const [archiveFields, setArchiveFields] = useState([
-    "title",
-    "content",
-    "type"
-  ])
-  const { type } = props.match.params
+    const [archive, setArchive] = useState([])
+    const [archiveFields, setArchiveFields] = useState([
+        "title",
+        "content",
+        "type"
+    ])
+    const { type } = props.match.params
 
-  const getData = query => {
-    let requestBody = {}
-    fetch(`http://localhost:4000/api/${type}`)
-      .then(response => {
-        return response.json()
-      })
-      .then(myJson => {
-        setArchive(myJson)
-      })
-  }
+    const getData = query => {
+        let requestBody = {}
+        fetch(`http://localhost:4000/api/${type}`)
+            .then(response => {
+                return response.json()
+            })
+            .then(myJson => {
+                setArchive(myJson)
+            })
+    }
 
-  const editData = query => {}
+    useEffect(() => {
+        getData()
+    }, [])
 
-  useEffect(() => {
-    getData()
-  }, [])
-
-  return archive ? (
-    <div className="Archive">
-      {archive.map(singleData => (
-        <Card
-          variant="outlined"
-          className={"SingleInArchive " + singleData.type}
-          key={singleData._id}
-        >
-          <CardContent>
-            {Object.keys(singleData).map(fieldKey => {
-              if (
-                fieldKey !== "_id" &&
-                fieldKey !== "id" &&
-                archiveFields.includes(fieldKey)
-              )
-                return (
-                  <Typography
-                    className={"field " + fieldKey}
-                  >{`${singleData[fieldKey]}`}</Typography>
-                )
-              return <></>
-            })}
-          </CardContent>
-          <CardActions>
-            <Link to={`/${type}/${singleData._id}`}>
-              <Button size="small">More</Button>
-            </Link>
-          </CardActions>
-        </Card>
-      ))}
-    </div>
-  ) : (
-    "loading"
-  )
+    return archive ? (
+        <div className="Archive grid grid3">
+            {archive.map(singleData => (
+                <div
+                    variant="outlined"
+                    className={"SingleInArchive " + singleData.type}
+                    key={singleData._id}
+                >
+                    <div>
+                        {Object.keys(singleData).map(fieldKey => {
+                            if (
+                                fieldKey !== "_id" &&
+                                fieldKey !== "id" &&
+                                archiveFields.includes(fieldKey)
+                            )
+                                return (
+                                    <div
+                                        className={"field " + fieldKey}
+                                    >{`${singleData[fieldKey]}`}</div>
+                                )
+                            return <></>
+                        })}
+                    </div>
+                    <div className="options">
+                        <Link to={`/${type}/${singleData._id}`}>
+                            <Button>More</Button>
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    ) : (
+        "loading"
+    )
 }
 
 export default Archive
