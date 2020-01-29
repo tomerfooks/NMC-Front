06c31b6e33515fbe5ce37200a722a7ef77386b64
tomerfooks <37155624+tomerfooks@users.noreply.github.com>
2020-01-29 21:40:14 +0200

    backtoschool

--- src/components/Single.js
+++ src/components/Single.js
@@ -4,25 +4,31 @@ const Single = props => {
     const [single, setSingle] = useState([])
     const { objectType, id } = props.match.params
     console.log(objectType)
-    const getData = query => {
+    const getData = () => {
         console.log('Getting data for single..')
         fetch(`http://localhost:4000/api/get/${objectType}/${id}`)
             .then(response => response.json())
             .then(json => setSingle(json))
     }
     useEffect(() => {
-        getData({})
+        if (single.length === 0) getData()
     }, [])
 
     return (
-        <div key={single.title} className="Single">
+        <div key={single.title} className='Single'>
             {Object.keys(single).map(fieldKey => {
                 if (fieldKey !== '_id' && fieldKey !== 'id')
-                    return (
-                        <div
-                            className={'field ' + fieldKey}
-                        >{`${single[fieldKey]}`}</div>
-                    )
+                    if (fieldKey === 'mainImage')
+                        return (
+                            <div className='mainImage'>
+                                <img src={'https://via.placeholder.com/300'} />
+                            </div>
+                        )
+                return (
+                    <div
+                        className={'field ' + fieldKey}
+                    >{`${single[fieldKey]}`}</div>
+                )
                 return <></>
             })}
         </div>
